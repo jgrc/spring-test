1a502da00fbcec367049804d2c9a66c09ab2dc9d
Juan G. Rodríguez <juan-gabriel.rodriguez@sezamo.es>
2022-04-26 10:48:46 +0200

    chore: added test profile to testing

--- src/test/java/com/example/bdd/CucumberSpringConfiguration.java
+++ src/test/java/com/example/bdd/CucumberSpringConfiguration.java
@@ -3,7 +3,9 @@ package com.example.bdd;
 import com.example.app.DemoApplication;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @CucumberContextConfiguration
 @SpringBootTest(classes = DemoApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CucumberSpringConfiguration {
